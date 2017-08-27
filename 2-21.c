6ab186f6ba13b324cdcf4e1d33169595daa5fd5a
PK160830 <31381702+PK160830@users.noreply.github.com>
2017-08-27 14:43:21 +0700

    Initial commit

---
+++ 2-21.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("My name is : %.2s\n","Kmitl");
+	printf("My point : %d\n",10+40+49);
+	printf("Grade : %c\n",'A');
+	printf("GPA : %.2f\n",3.99);
+	printf("78 = %c\n",78);
+	printf("%d-%d-%d",16,8,30); // 16-8-30
+	return 0;
+}
---
+++ 2-24.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	printf("Age = %d, GPA = %.2f\n",30,4.00);
+	printf("Programming: %.1f\nMechanice: %.1f",4.0,3.5);
+	return 0;
+}
