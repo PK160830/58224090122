46a30c8b548bf9d8cfcf3abf0078de319f957340
PK160830 <31381702+PK160830@users.noreply.github.com>
2017-09-07 08:07:08 +0700

    3-22

---
+++ 3-22.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	char subject[12] ="Programming";
+	char nick[4] ="Com";
+	char nick_1[4] ={'c','o','m','\0'};
+	char name[] = "Jirasak";
+	printf("%s\n",subject);
+	printf("%s\n",nick);
+	printf("%s\n",nick_1);
+	printf("%s\n",name);
+}
